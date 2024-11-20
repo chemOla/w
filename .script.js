af767bf4dcb1be5736cd02f0cc22e4208203ab68
chemOla <ola44373@gmail.com>
2024-11-20 16:18:36 +0000

    Add files via upload

---
+++ .script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("show-btn").onclick = function() {
+    var content = document.getElementById("hidden-content");
+    content.style.display = content.style.display === "none" ? "block" : "none"; 
+};
